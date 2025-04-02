192e1591707b084583e3fbea81c497e8e003c023
Shukra Tamang <shukratamang.npl@gmail.com>
2025-04-03 09:52:49 +1100

    build search functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { IoSearch } from "react-icons/io5";
 
 const Search = ({ searchTerm, setSearchTerm }) => {
   return (
     <div className="search">
-      <div>
-        {/* search icon */}
+      <div className="flex items-center justify-center">
+        <IoSearch className="text-white" />
         <input
           type="text"
           placeholder="Search movies..."
